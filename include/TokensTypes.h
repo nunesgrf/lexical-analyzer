a115cf3b4e448b66358157e0d04964f586732c51
Gabriel Nunes <nunesgrf@gmail.com>
2021-02-15 01:26:31 -0300

    Add static token type generator

---
+++ include/TokensTypes.h
@@ -0,0 +1,25 @@
+#ifndef TOKEN_TYPES_H
+#define TOKEN_TYPES_H
+
+#define GENERATE_ENUM(ENUM) ENUM,
+#define GENERATE_STRING(STRING) #STRING,
+#define FOREACH_TOKEN(TOKEN)\
+            TOKEN(TEST_1)\
+            TOKEN(TEST_2)\
+            TOKEN(TEST_3)\
+
+#include <stdio.h>
+
+typedef enum Token {
+    FOREACH_TOKEN(GENERATE_ENUM)
+} Token;
+
+static const char *TokenString[] = {
+    FOREACH_TOKEN(GENERATE_STRING)
+};
+
+void print(Token token) {
+    printf("<%s>", TokenString[token]);
+}
+
+#endif
